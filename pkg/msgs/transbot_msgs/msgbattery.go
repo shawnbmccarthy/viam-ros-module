32f1bde13ed2c0fae3922bc5fef618c24d0173c8
shawnbmccarthy <sbmccarth@gmail.com>
2023-07-07 15:51:12 -0400

    added battery sensor

--- pkg/msgs/transbot_msgs/msgbattery.go
+++ pkg/msgs/transbot_msgs/msgbattery.go
@@ -2,9 +2,11 @@ package transbot_msgs
 
 import (
 	"github.com/bluenviron/goroslib/v2/pkg/msg"
+	"github.com/bluenviron/goroslib/v2/pkg/msgs/std_msgs"
 )
 
 type Battery struct {
 	msg.Package `ros:"transbot_msgs"`
+	Header      std_msgs.Header
 	Voltage     float32
 }
